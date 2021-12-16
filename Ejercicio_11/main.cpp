b9d94b979b8cdc957808df32102e8a7aaa35f232
Agiraldo07 <96264485+Agiraldo07@users.noreply.github.com>
2021-12-16 15:29:42 -0500

    Add files via upload

---
+++ Ejercicio_11/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num,count=1,mult=1;
+    cout<<"Escribe un numero:";
+    cin>>num;
+    while(count<=10){
+        mult=num*count;
+        cout<<count<<'*'<<num<<'='<<mult<<endl;
+        count++;
+    }
+    return 0;
+}
---
+++ Ejercicio_3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cout<<"Escribe un numero:";
+    cin>>num1;
+    cout<<"Escribe un numero:";
+    cin>>num2;
+    if(num1>num2){
+        cout<<"El mayor es:"<<num1<<endl;
+    }
+    else{
+        cout<<"El mayor es:"<<num2<<endl;
+    }
+    return 0;
+}
